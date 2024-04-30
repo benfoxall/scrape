1559eb014f604a78909a1122798d9d20b596489e
Ben Foxall <benfoxall@gmail.com>
2024-04-30 20:19:50 +0000

    replace bash scripts with js

--- ui/fetch-data.js
+++ ui/fetch-data.js
@@ -1,29 +1,18 @@
-#!/usr/bin/node
-
 import { readdir, readFile, writeFile } from 'node:fs/promises';
+import { promisify } from 'node:util';
+import { exec } from 'node:child_process';
 import { parse, join } from 'path';
 import { JSDOM } from "jsdom";
 
-const directory = './collected'
-const destination = './public/data.json'
+const execP = promisify(exec);
 
-const files = await readdir(directory);
+const file = "../data/hacker-news.html"
+const destination = './public/hn.json'
+
 const output = [];
 
-// parse a blah/1234.html
-const timestamp = (path) => parseInt(parse(path).name, 10);
-
-files.sort(
-    // numerical sort, oldest to newest
-    (a, b) => timestamp(a) - timestamp(b)
-)
-
-
-for (const file of files) {
-    const time = timestamp(file);
-    console.log(file, timestamp(file));
-
-    const contents = await readFile(join(directory, file), 'utf8');
+for await (const { time, contents } of historyOf(file)) {
+    console.log("reading ", time);
 
     const { window: { document } } = new JSDOM(contents);
 
@@ -59,8 +48,27 @@ for (const file of files) {
     })
 }
 
-
-
 await writeFile(destination, JSON.stringify(output, null, 2));
 
+console.log(`Wrote ${output.length} entries to ${destination}`)
 
+
+
+
+
+// iterate across the history of a file
+async function* historyOf(file) {
+    const { stdout } = await execP(`git log -n 1000 --pretty=format:"%H %at" -- "${file}"`);
+    const entries = stdout.split('\n').map(row => row.split(' '));
+    for (const [commit, timestr] of entries) {
+
+        const time = new Date(parseFloat(timestr) * 1000)
+
+        const { stdout } = await execP(`git show "${commit}:${file}"`)
+
+        yield {
+            time,
+            contents: stdout
+        }
+    }
+}
