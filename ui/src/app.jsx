4f00aadc27ab1a7bbf2ba73311b1abb15ee85eef
Ben Foxall <benfoxall@gmail.com>
2024-05-01 18:44:20 +0000

    Switch back to hn.json

--- ui/src/app.jsx
+++ ui/src/app.jsx
@@ -1,11 +1,17 @@
 import "./app.css";
 import { createRoot } from "react-dom/client";
 import { View } from "./View";
-import hn from "../../hacker-news.json";
 
 const root = createRoot(document.getElementById("app"));
 
-root.render(<View data={hn} />);
+try {
+  const res = await fetch("hn.json");
+  const hn = await res.json();
+
+  root.render(<View data={hn} />);
+} catch (e) {
+  root.render(<h1>Error, unable to load</h1>);
+}
 
 if (DEV) {
   new EventSource("/esbuild").addEventListener("change", () =>
