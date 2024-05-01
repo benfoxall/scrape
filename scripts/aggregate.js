3c7f76f1e3b4892e4acf0801f75f493e6e896f85
Ben Foxall <benfoxall@gmail.com>
2024-05-01 08:13:08 +0000

    Rename output

--- scripts/aggregate.js
+++ scripts/aggregate.js
@@ -2,7 +2,7 @@ import { writeFile } from 'node:fs/promises';
 import { hn2json, historyOf } from './util.js';
 
 const file = "data/hacker-news.html"
-const destination = 'data/hacker-news.aggregate.json'
+const destination = 'data/hn.json'
 
 const aggregated = [];
 
@@ -18,7 +18,7 @@ for await (const { time, contents } of historyOf(file)) {
     if (aggregated.length > 500) break;
 }
 
-await writeFile(destination, JSON.stringify(aggregated, null, 2));
+await writeFile(destination, JSON.stringify(aggregated));
 
 console.log(`Wrote ${aggregated.length} entries to ${destination}`)
 
