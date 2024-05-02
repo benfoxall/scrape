5af19ddb6cf82450edac6358bd0117ad4029c2e5
Ben Foxall <benfoxall@gmail.com>
2024-05-02 07:04:14 +0000

    UI tweaking

--- ui/src/View.jsx
+++ ui/src/View.jsx
@@ -1,40 +1,57 @@
-import { useState } from "react";
+import { useState, useRef, useLayoutEffect, useMemo } from "react";
+import { formatDistanceToNow } from "date-fns";
 
 export const View = ({ data }) => {
   const [idx, setIdx] = useState(0);
+  const [hov, setHov] = useState(0);
 
   const current = data[idx];
 
   return (
     <>
-      <input
-        type="range"
-        value={idx}
-        max={data.length - 1}
-        onChange={(e) => setIdx(e.target.valueAsNumber)}
-      />
-      {idx + 1} / {data.length}
+      <label>
+        hacker news
+        {current && ` – ${formatDistanceToNow(new Date(current.at))} ago`}
+        <br />
+        <input
+          type="range"
+          value={idx}
+          max={data.length - 1}
+          onChange={(e) => setIdx(e.target.valueAsNumber)}
+        />
+      </label>
       {current && (
         <>
-          <h3>{new Date(current.at).toLocaleString()}</h3>
-
-          <ol>
-            {current.entries.map(
-              ({ id, rank, score, text, url, user, created, comments }) => (
-                <li key={id}>
-                  <h4>
-                    <a href={url}>{text}</a>
-                  </h4>
-                  <p>
-                    {score} points by {user},{" "}
-                    <a href={`https://news.ycombinator.com/item?id=${id}`}>
-                      {comments} comments
-                    </a>
-                  </p>
-                </li>
-              )
-            )}
-          </ol>
+          <div className="time-graph">
+            <ol>
+              {current.entries.map(
+                ({ id, rank, score, text, url, user, created, comments }) => (
+                  <li
+                    key={id}
+                    onClick={() => setHov(id)}
+                    className={hov === id ? "active" : ""}
+                  >
+                    <h4>
+                      <a
+                        href={new URL(
+                          url,
+                          "https://news.ycombinator.com"
+                        ).toString()}
+                      >
+                        {text}
+                      </a>
+                    </h4>
+                    <p>
+                      {score} points by {user},{" "}
+                      <a href={`https://news.ycombinator.com/item?id=${id}`}>
+                        {comments} comments
+                      </a>
+                    </p>
+                  </li>
+                )
+              )}
+            </ol>
+          </div>
         </>
       )}
     </>
